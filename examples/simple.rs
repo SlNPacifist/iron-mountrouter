8d980655815e5ee6669cce85ef2d2c57ab6657d9
slnpacifist <slnpacifist@gmail.com>
2015-10-10 14:15:11 +0600

    Updated project name and settings Route-recognizer now points at custom repo Added option to mount route so further handlers only see part of the route Added simple example

---
+++ examples/simple.rs
@@ -0,0 +1,28 @@
+extern crate iron;
+extern crate iron_mountrouter;
+
+// To run, $ cargo run --example simple
+// To use, go to http://127.0.0.1:3000/test
+
+use iron::{Iron, Request, Response, IronResult};
+use iron::status;
+use iron_mountrouter::{Router};
+
+fn main() {
+    let mut router = Router::new();
+    router.add_route("/", handler, false);
+    router.add_route("/:query/:sub-query", handler, false);
+    
+    let mut internal_router = Router::new();
+    internal_router.add_route("/delete/:key/", handler, false);
+    internal_router.add_route("/update/:key/", handler, false);
+    router.add_route("/page/:num/", internal_router, true);
+
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>()
+            .unwrap();
+        Ok(Response::with((status::Ok, format!("Url: {:?}\nQuery params: {:?}", req.url.path, *query))))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,31 @@
 extern crate iron;
 extern crate route_recognizer as recognizer;
+extern crate url;
 
 use std::error::Error;
 use std::fmt;
+use std::collections::BTreeMap;
 
 use iron::{Request, Response, Handler, IronResult, IronError};
 use iron::status;
 use iron::typemap::Key;
 
 use recognizer::Router as Recognizer;
-use recognizer::{Match, Params};
+use recognizer::Params as RecognizerParams;
+use recognizer::Match;
 
+use url::parse_path;
+
+const LEFTOVER_PARAM_NAME: &'static str = "mountrouter_leftover";
+pub type Params = BTreeMap<String, String>;
+
+struct RouteParams {
+	handler: Box<Handler>,
+	is_mounted: bool,
+}
 
 pub struct Router {
-    recognizer: Recognizer<Box<Handler>>
+    recognizer: Recognizer<RouteParams>
 }
 
 impl Router {
@@ -23,14 +35,43 @@ impl Router {
         }
     }
 
-    pub fn add_route<H, S>(&mut self, route: S, handler: H) -> &mut Router
+    pub fn add_route<H, S>(&mut self, route: S, handler: H, is_mounted: bool) -> &mut Router
     where H: Handler, S: AsRef<str> {
-        self.recognizer.add(route.as_ref(), Box::new(handler));
+    	let params = RouteParams {
+    		handler: Box::new(handler),
+    		is_mounted: is_mounted
+    	};
+    	if is_mounted {
+    		let r = route.as_ref();
+    		let mut modified_route = String::with_capacity(r.len() + 2 + LEFTOVER_PARAM_NAME.len());
+    		modified_route.push_str(r);
+    		if !r.ends_with('/') {
+				modified_route.push('/');
+			}
+    		modified_route.push('*');
+    		modified_route.push_str(LEFTOVER_PARAM_NAME);
+	        self.recognizer.add(&modified_route, params);
+    	} else {
+	        self.recognizer.add(route.as_ref(), params);
+    	}
         self
     }
 
-    fn recognize(&self, path: &str) -> Option<Match<&Box<Handler>>> {
+    fn recognize(&self, path: &str) -> Option<Match<&RouteParams>> {
         self.recognizer.recognize(path).ok()
+    }
+    
+    fn append_params(req: &mut Request, params: &RecognizerParams) {
+    	if !req.extensions.contains::<Router>() {
+			req.extensions.insert::<Router>(Params::new());
+    	}
+    	let p = req.extensions.get_mut::<Router>().unwrap();
+    	for name in params.keys() {
+    		if name != LEFTOVER_PARAM_NAME {
+    			p.insert(name.to_string(), params[name.as_ref()].clone());
+			}
+    	}
+    	println!("{:?}", p);
     }
 }
 
@@ -41,8 +82,13 @@ impl Handler for Router {
         let path = req.url.path.join("/");
         match self.recognize(&path) {
         	Some(matched) => {
-	            req.extensions.insert::<Router>(matched.params);
-	            matched.handler.handle(req)
+	            if matched.handler.is_mounted {
+	            	let leftover = "/".to_string() + &matched.params[LEFTOVER_PARAM_NAME];
+	            	let (new_path, _, _) = parse_path(&leftover).unwrap();
+	            	req.url.path = new_path;
+	            }
+            	Router::append_params(req, &matched.params);
+	            matched.handler.handler.handle(req)
         	},
         	None => Err(IronError::new(NoRoute, status::NotFound))
         }
