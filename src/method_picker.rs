7a17c0bbd52bdd8f32bb0321b65d77836c3aff76
slnpacifist <slnpacifist@gmail.com>
2015-10-12 22:22:01 +0600

    Added documentation

--- src/method_picker.rs
+++ src/method_picker.rs
@@ -8,22 +8,36 @@ use iron::method::Method;
 type PickerHandlers = Vec<(Option<Method>, Box<Handler>)>;
 
 
+/// `MethodPicker` provides an interface to apply different handlers for different http methods as
+/// middleware for the Iron framework.
 pub struct MethodPicker {
 	handlers: PickerHandlers
 }
 
 impl MethodPicker {
+    /// Construct new `MethodPicker`.
+    ///
+    /// ```
+    /// # use iron_mountrouter::MethodPicker;
+    /// let picker = MethodPicker::new();
+    /// ```
 	pub fn new() -> MethodPicker {
 		MethodPicker {
 			handlers: PickerHandlers::new()
 		}
 	}
 	
+	/// Adds new handler to picker
+	///
+	/// `method` is any Iron's http method.
+	///
+	/// `handler` will be used for specified method
 	pub fn add<H: Handler>(&mut self, method: Method, handler: H) -> &mut MethodPicker {
 		self.handlers.push((Some(method), Box::new(handler)));
 		self
 	}
 	
+	/// Adds default handler to picker which will be used if no method matched
 	pub fn default<H: Handler>(&mut self, handler: H) -> &mut MethodPicker {
 		self.handlers.push((None, Box::new(handler)));
 		self
@@ -45,6 +59,7 @@ impl Handler for MethodPicker {
 }
 
 
+/// `NoMethod` is error type returned to Iron if no handler was matched in MethodPicker
 #[derive(Debug)]
 pub struct NoMethod;
 
--- src/mount_router.rs
+++ src/mount_router.rs
@@ -13,6 +13,12 @@ use recognizer::Match;
 use url::parse_path;
 
 const LEFTOVER_PARAM_NAME: &'static str = "mountrouter_leftover";
+
+/// `Params` is stored in request extensions and gives access to captured dynamic parameters
+///
+/// ```ignore
+/// let ref params = req.extensions.get::<Router>().unwrap()
+/// ```
 pub type Params = BTreeMap<String, String>;
 
 struct RouteParams {
@@ -20,17 +26,80 @@ struct RouteParams {
 	is_mounted: bool,
 }
 
+/// `Router` provides an interface to apply different handlers to different routes as middleware for
+/// the Iron framework.
 pub struct Router {
     recognizer: Recognizer<RouteParams>
 }
 
 impl Router {
+    /// Construct new `Router`.
+    ///
+    /// ```
+    /// # use iron_mountrouter::Router;
+    /// let router = Router::new();
+    /// ```
     pub fn new() -> Router {
         Router {
             recognizer: Recognizer::new()
         }
     }
 
+    /// Add a new route to a `Router`, matching given pattern.
+    ///
+    /// `route` is a pattern supported by [route recognizer]
+    /// (https://crates.io/crates/route-recognizer):
+    ///
+    /// string like `/static/page/here` is matched entirely;
+    ///
+    /// `:name` is a dynamic param, matching any string that does not contain slash `/`, i.e.
+    ///
+    /// `/page/:page-num/content` will match `/page/17/content` or `/page/abbra-t/content`
+    /// but not `/page/17/5/content`.
+    ///
+    /// Colon only works at the start of the segment so `/page-:num` works like usual string match
+    /// and will not match `/page-5`.
+    ///
+    /// Currently there is no way to set param type so it catches only strings and does not perform
+    /// any additional checks.
+    ///
+    /// `*name` is a dynamic wildcard param, matching any string (even those containing slashes`/`).
+    /// `/redirect/to/*path` will match `/redirect/to/example.com/example`.
+    ///
+    /// Captured parameters are stored in [Params](type.Params.html) type and can be accessed
+    /// in request extensions:
+    ///
+    /// ```ignore
+    /// let ref params = req.extensions.get::<Router>().unwrap()
+    /// ```
+    ///
+    /// Unwrap is safe here because router always adds extension if route is matched, even if no
+    /// params were captured.
+    ///
+    /// `handler` is any [Iron handler]
+    /// (http://ironframework.io/doc/iron/middleware/trait.Handler.html). Working with mountrouter
+    /// you will most often use [MethodPicker](struct.MethodPicker.html) to pick appropriate
+    /// handler for given http method.
+    ///
+    /// If `is_mounted` is true, router will strip matched part of the request url. Unlike [Mount]
+    /// (http://ironframework.io/doc/mount/index.html) middleware from the Iron's core bundle, this
+    /// parameter allows you to strip dynamical (e.g. with dynamic params) strings from request
+    /// path. Original url is stored in request extensions using `OriginalUrl`. For example,
+    ///
+    /// ```ignore
+    /// let mut router = Router::new();
+    /// router.add_route("/book/:page/", handler, true);
+    /// fn handler(req: &mut Request) -> IronResult<Response> {
+    ///     // for path "/book/17/page/3"
+    ///
+    ///     // ["page", "3"]
+    ///     println!("{:?}", req.url.path);
+    ///     // ["book", "17", "page", "3"]
+    ///     println!("{:?}", req.extensions.get::<OriginalUrl>().unwrap().path);
+    /// }
+    /// ```
+    /// If `is_mounted` is set, route is forced to end with slash: slash is appended to the end of
+    /// route if it is not there.
     pub fn add_route<H, S>(&mut self, route: S, handler: H, is_mounted: bool) -> &mut Router
     where H: Handler, S: AsRef<str> {
     	let params = RouteParams {
@@ -93,10 +162,19 @@ impl Handler for Router {
     }
 }
 
+
+/// `OriginalUrl` serves as a key in request extensions to original url if router strips url for
+/// mounted handlers.
+///
+/// ```ignore
+/// let ref original_url = req.extensions.get::<OriginalUrl>()
+/// ```
 #[derive(Copy, Clone)]
 pub struct OriginalUrl;
 impl Key for OriginalUrl { type Value = Url; }
 
+
+/// `NoRoute` is error type returned to Iron if no route was matched in Router
 #[derive(Debug)]
 pub struct NoRoute;
 
