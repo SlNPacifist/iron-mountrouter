1e96accc66534714d56315afdbbd641ee916c516
slnpacifist <slnpacifist@gmail.com>
2015-10-15 17:40:00 +0600

    Updated docs

--- implementors/typemap/trait.Key.js
+++ implementors/typemap/trait.Key.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['iron_mountrouter'] = ["impl Key for <a class='struct' href='iron_mountrouter/struct.Router.html' title='iron_mountrouter::Router'>Router</a>","impl Key for <a class='struct' href='iron_mountrouter/struct.OriginalUrl.html' title='iron_mountrouter::OriginalUrl'>OriginalUrl</a>",];
+implementors['iron_mountrouter'] = ["impl Key for <a class='struct' href='iron_mountrouter/struct.Router.html' title='iron_mountrouter::Router'>Router</a>","impl Key for <a class='struct' href='iron_mountrouter/struct.StrippedUrl.html' title='iron_mountrouter::StrippedUrl'>StrippedUrl</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
