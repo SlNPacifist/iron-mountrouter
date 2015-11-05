7dd1aae791bbbded31477aa0621b71206fb96650
slnpacifist <slnpacifist@gmail.com>
2015-11-05 22:21:23 +0800

    Updated docs

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['iron_mountrouter'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='iron_mountrouter/struct.StrippedUrl.html' title='iron_mountrouter::StrippedUrl'>StrippedUrl</a>",];
+implementors['iron_mountrouter'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='iron_mountrouter/struct.StrippedUrl.html' title='iron_mountrouter::StrippedUrl'>StrippedUrl</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['iron_mountrouter'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='iron_mountrouter/struct.NoMethod.html' title='iron_mountrouter::NoMethod'>NoMethod</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='iron_mountrouter/struct.NoRoute.html' title='iron_mountrouter::NoRoute'>NoRoute</a>",];
+implementors['iron_mountrouter'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='iron_mountrouter/struct.NoMethod.html' title='iron_mountrouter::NoMethod'>NoMethod</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='iron_mountrouter/struct.NoRoute.html' title='iron_mountrouter::NoRoute'>NoRoute</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
